9485ecff1b817cc15b32842b6159b099341c448f
mariohuq <15021607+mariohuq@users.noreply.github.com>
2023-02-03 00:18:02 +0300

    implement freq, use 1251 encoded alphabet for generation

--- L0/main.cpp
+++ L0/main.cpp
@@ -5,9 +5,14 @@
 #include <ostream>
 #include <random>
 #include <fstream>
+#include "sstream"
 #include <iostream>
+#include <map>
+#include <algorithm>
 
-void generate_file(const std::vector<std::string>& alphabet,
+using FrequencyMap = std::map<char, size_t>;
+
+void generate_file(const std::string& alphabet,
                    size_t characters_count,
                    std::ostream& os,
                    std::mt19937& generator) {
@@ -17,14 +22,32 @@ void generate_file(const std::vector<std::string>& alphabet,
     }
 }
 
+FrequencyMap count_characters(std::istream& is) {
+    FrequencyMap res{};
+    char c;
+    while (is >> c) {
+        ++res[c];
+    }
+    return res;
+}
+
+std::vector<char> alphabet_sorted(const FrequencyMap& frequencies) {
+    std::vector<char> res{};
+    res.reserve(frequencies.size());
+    for (auto [c, _] : frequencies) {
+        res.push_back(c);
+    }
+    std::sort(res.begin(), res.end(), [&](auto left, auto right) {
+        return frequencies.at(left) > frequencies.at(right);
+    });
+    return res;
+}
+
 int main() {
     std::mt19937 gen{std::random_device{}()};
     std::fstream out{"10k.in", std::ios::out};
-    generate_file({"С", "М", "И", "Ъ", "Е", "Т",
-                   "с", "м", "и", "ъ", "е", "т",
-                   " ",
-                   "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                   "<", "."}, 10'000,out,gen);
+    std::string alphabet{(std::ostringstream{} << std::fstream {"alphabet.txt"}.rdbuf()).str()};
+    generate_file(alphabet, 10'000,out,gen);
 //    - B — Алгоритм Хаффмена
     return 0;
 }
