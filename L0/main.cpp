553cfe995a94bae216233a6523a73b15eeaed262
mariohuq <15021607+mariohuq@users.noreply.github.com>
2023-02-04 16:52:59 +0300

    impl the coding

--- L0/main.cpp
+++ L0/main.cpp
@@ -5,12 +5,14 @@
 #include <ostream>
 #include <random>
 #include <cstring>
-#include "sstream"
+#include <sstream>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <algorithm>
 #include <bitset>
 #include <cassert>
+#include <numeric>
 
 using freq_t = size_t;
 using FrequencyMap = std::map<char, freq_t>;
@@ -28,7 +30,7 @@ void generate_file(const std::string &alphabet,
 FrequencyMap count_characters(std::istream &is) {
     FrequencyMap res{};
     char c;
-    while (is >> c) {
+    while (is.get(c)) {
         ++res[c];
     }
     return res;
@@ -71,6 +73,8 @@ struct Code {
 
 static_assert(sizeof(Code) == 4);
 
+using AlphabetCoding = std::map<char, Code>;
+
 std::vector<Code> Huffman(std::vector<freq_t> probabilities) {
     std::vector<Code> codes;
     codes.reserve(probabilities.size());
@@ -100,6 +104,64 @@ std::vector<Code> Huffman(std::vector<freq_t> probabilities) {
     MyHuffman(MyHuffman);
     return codes;
 }
+
+struct EncodingStats {
+    size_t input_size;
+    size_t output_size;
+};
+
+double coding_price(const std::vector<Code>& codes, const std::vector<freq_t>& frequencies) {
+    size_t res = std::inner_product(codes.begin(), codes.end(), frequencies.begin(),
+                                    size_t{}, std::plus<size_t>{}, [](Code code, freq_t freq) {
+        return code.length * freq;
+    });
+    size_t one = std::accumulate(frequencies.begin(), frequencies.end(), size_t{});
+    return static_cast<double>(res) / static_cast<double>(one);
+}
+
+EncodingStats encode(std::istream& is, std::ostream& os, const AlphabetCoding& coding) {
+    // Header
+    size_t alphabet_size = coding.size();
+    os.write(reinterpret_cast<char*>(&alphabet_size), sizeof(alphabet_size));
+    for (auto [character, code]: coding) {
+        os.write(&character, sizeof(char));
+        os.write(reinterpret_cast<char*>(&code), sizeof(Code));
+    }
+    EncodingStats result{};
+    result.output_size = sizeof(alphabet_size) + alphabet_size * (sizeof(char) + sizeof(Code));
+    char input_buffer;
+    size_t nbits = 0;
+    char current_byte = '\0';
+
+    auto bitout = [&](Code code) {
+        for (; code.length > 0; code.length--, code.value >>= 1) {
+            /* shift current byte left one */
+            current_byte <<= 1;
+            /* put a one on the end of this byte if b is '1' */
+            current_byte |= code.value & 1;
+            /* one more bit */
+            nbits++;
+            /* enough bits?  write out the byte */
+            if (nbits == 8) {
+                os.put(current_byte);
+                result.output_size++;
+                nbits = 0;
+                current_byte = '\0';
+            }
+        }
+    };
+    result.input_size = 0;
+    while (is.get(input_buffer)) {
+        bitout(coding.at(input_buffer));
+        result.input_size++;
+    }
+    if (nbits != 0) {
+        bitout(Code{0, static_cast<unsigned char>(8 - nbits)});
+    }
+    return result;
+}
+
+
 void test_haffman();
 
 int main(int argc, const char *argv[]) {
@@ -111,8 +173,28 @@ int main(int argc, const char *argv[]) {
         generate_file(alphabet, 10'000, std::cout, gen);
         return EXIT_SUCCESS;
     }
-    if (argc == 2 && strcmp(argv[1], "encode_huffman") == 0) {
-        count_characters(std::cin);
+    if (argc == 3 && strcmp(argv[1], "encode_huffman") == 0) {
+        std::ifstream input{argv[2]};
+        auto freqs = count_characters(input);
+        auto alphabet = alphabet_sorted(freqs);
+        std::vector<freq_t> frequencies;
+        frequencies.reserve(alphabet.size());
+        std::transform(alphabet.begin(), alphabet.end(), std::back_inserter(frequencies),
+                       [&](char alpha){
+            return freqs[alpha];
+        });
+        auto codes = Huffman(frequencies);
+        std::cerr << "цена кодирования = " << coding_price(codes, frequencies) << '\n';
+        std::map<char, Code> encoding{};
+        std::transform(alphabet.begin(), alphabet.end(), codes.begin(),
+                       std::inserter(encoding, encoding.end()),
+                       std::make_pair<const char&, const Code&>);
+        input.clear();
+        input.seekg(0, std::ios::beg); // rewind
+        auto stats = encode(input, std::cout, encoding);
+        std::cerr
+            << "коэффициент сжатия = "
+            << static_cast<double>(stats.output_size) / static_cast<double>(stats.input_size) << '\n';
         return EXIT_SUCCESS;
     }
     return EXIT_FAILURE;
