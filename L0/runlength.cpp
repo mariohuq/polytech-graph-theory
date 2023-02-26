f1b3ca4f9d2e4dff0978e841fee5a0a10efd3dfe
mariohuq <15021607+mariohuq@users.noreply.github.com>
2023-02-27 01:28:45 +0300

    handle overbytes

--- L0/runlength.cpp
+++ L0/runlength.cpp
@@ -12,15 +12,31 @@ struct Code {
 rle::EncodingStats rle::encode(std::istream& is, std::ostream& os) {
     Code current;
     rle::EncodingStats stats{};
+    size_t overbytes;
     while (is.get(current.byte)) {
         current.count = 1;
+        overbytes = 0;
         while (is.peek() == current.byte) {
             current.count++;
+            if (current.count == 0) {
+                overbytes++;
+            }
             is.get();
         }
+        if (overbytes > 0) {
+            Code over = {current.byte, 0};
+            for (size_t i = 0; i < overbytes; ++i) {
+                os.write(reinterpret_cast<char*>(&over), sizeof(over));
+                stats.output_size += 2;
+                stats.input_size += 0x100;
+            }
+            if (current.count == 0) {
+                continue;
+            }
+        }
         os.write(reinterpret_cast<char*>(&current), sizeof(current));
-        stats.input_size += current.count;
         stats.output_size += 2;
+        stats.input_size += current.count;
     }
     return stats;
 }
