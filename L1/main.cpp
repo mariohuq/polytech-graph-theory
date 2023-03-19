0e42ddfa78a9b906566a97a4639e732945bf1679
mariohuq <15021607+mariohuq@users.noreply.github.com>
2023-03-20 00:41:22 +0300

    make some insignif progress

--- L1/main.cpp
+++ L1/main.cpp
@@ -4,10 +4,12 @@
 
 #include <iostream>
 #include <map>
+#include <cstdlib>
+#include <cassert>
 #include "polya_dist.h"
 
-int main() {
-    auto dis = polya_1_distribution<>(20, 20, 1, 5);
+void test_polya() {
+    auto dis = polya_1<>(20, 20, 1, 5);
     std::random_device rd;
     std::mt19937 gen(rd());
     std::map<int, int> map;
@@ -18,3 +20,43 @@ int main() {
     for(const auto& [num, count] : map)
         std::cout << num << "\t" << count << "\n";
 }
+
+using adjacency_matrix = std::vector<std::vector<bool>>;
+//Д. 1.3. Алгоритм построения случайного ориентированного бесконтурного графа, Хаг
+adjacency_matrix generate_acyclic_convex(size_t nVertices, size_t nEdges) {
+    auto dis = polya_1<int>(20, 20, 1, nVertices * (nVertices - 1) / 2 - 1);
+    auto split = [](int k) -> div_t {
+        int i = (-1 + sqrt(8 * k + 1)) / 2;
+        return {k - i*(i+1)/2, i+1};
+    };
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    adjacency_matrix result{nVertices, std::vector<bool>(nVertices)};
+
+    for (int k = 0; k < nEdges; k++) {
+        // i < j by construction
+        auto [i, j] = split(dis(gen));
+        assert(i < j);
+        assert(j < nVertices);
+        if (result[i][j]) {
+            k--;
+            continue;
+        }
+        result[i][j] = true;
+    }
+    return result;
+}
+
+int main() {
+    int n = 10;
+    auto g = generate_acyclic_convex(10, 18);
+    for (const auto& row : g) {
+        for (const auto& elem: row) {
+            std::cout << elem << ',';
+        }
+        std::cout << '\n';
+    }
+    return 0;
+}
+
--- L1/polya_dist.h
+++ L1/polya_dist.h
@@ -3,152 +3,11 @@
 //
 
 #pragma once
-
 #include <random>
 #include <tuple>
 
-//template<class IntType = int>
-//struct polya_1_distribution {
-//    static_assert(std::is_integral<IntType>::value,
-//                  "result_type must be an integral type");
-//
-//    /** The type of the range of the distribution. */
-//    using result_type = IntType;
-//
-//    /** Parameter type. */
-//    struct param_type {
-//        using distribution_type = polya_1_distribution<IntType>;
-//        IntType black_n, red_n, c;
-//        IntType size;
-//        double p0; // precalculated for x = 0
-//
-//        param_type(IntType black_n, IntType red_n, IntType c, IntType size)
-//                : black_n{black_n}, red_n{red_n}, c{c}, size{size}, p0{1} {
-//            for (int i = 0; i < p.size; i++) {
-//                p0 *= (p.red_n + i * p.c) / (p.black_n + p.red_n + i * p.c);
-//            }
-//        }
-//
-//        friend bool
-//        operator==(const param_type &lhs, const param_type &rhs) {
-//            return std::tie(lhs.black_n, lhs.red_n, lhs.c, lhs.size)
-//                == std::tie(rhs.black_n, rhs.red_n, rhs.c, rhs.size);
-//        }
-//
-//        friend bool
-//        operator!=(const param_type &lhs, const param_type &rhs) { return !(lhs == rhs); }
-//    };
-//
-//    polya_1_distribution(IntType black_n, IntType red_n, IntType c, IntType size) : m_param{black_n, red_n, c, size} {
-//    }
-//
-//    /**
-//     * @brief Resets the distribution state.
-//     */
-//    void reset() { }
-//
-//    /**
-//     * @brief Returns the parameter set of the distribution.
-//     */
-//    param_type param() const { return m_param; }
-//    /**
-//     * @brief Sets the parameter set of the distribution.
-//     * @param _param The new parameter set of the distribution.
-//     */
-//    void param(const param_type& _param) { m_param = _param; }
-//    /**
-//     * @brief Returns the greatest lower bound value of the distribution.
-//     */
-//    result_type min() const { return {0}; }
-//
-//    /**
-//     * @brief Returns the least upper bound value of the distribution.
-//     */
-//    result_type max() const { return m_param.size; }
-//
-//    /**
-//       * @brief Generating functions.
-//       */
-//    template<typename UniformRandomNumberGenerator>
-//	result_type operator()(UniformRandomNumberGenerator& urng)
-//	{ return this->operator()(urng, m_param); }
-//
-//    template<typename UniformRandomNumberGenerator>
-//	result_type operator()(UniformRandomNumberGenerator& urng, const param_type& p) {
-//        constexpr std::uniform_real_distribution<> dis(0.0, 1.0);
-//        constexpr auto alpha = [&p](result_type x) {
-//            return (p.size + 1 - x)
-//                * (p.black_n + (x - 1) * p.c)
-//                / (x * (p.red_n + (p.size - x) * p.c));
-//        };
-//
-//        double r = dis(urng);
-//        double probab = p.p0;
-//        for (IntType x = 0; x < p.size; ++x, probab *= alpha(x)) {
-//            r -= probab;
-//            if (r < 0) {
-//                return x;
-//            }
-//        }
-//        return p.size;
-//    }
-//
-//    /**
-//     * @brief Return true if two discrete distributions have the same
-//     *        parameters.
-//     */
-//    friend bool operator==(const polya_1_distribution& lhs, const polya_1_distribution& rhs)
-//    { return lhs.m_param == rhs.m_param; }
-//
-//    /**
-//     * @brief Inserts a %polya_1_distribution random number distribution
-//     * @p x into the output stream @p os.
-//     *
-//     * @param os An output stream.
-//     * @param x  A %polya_1_distribution random number distribution.
-//     *
-//     * @returns The output stream with the state of @p x inserted or in
-//     * an error state.
-//     */
-//    template<typename IntType1>
-//	friend std::ostream&
-//	operator<<(std::ostream& os,
-//		   const polya_1_distribution<IntType1>& x) {
-//        return os
-//            << x.m_param.red_n << ' '
-//            << x.m_param.black_n << ' '
-//            << x.m_param.c << ' '
-//            << x.m_param.size;
-//    }
-//
-//    /**
-//     * @brief Extracts a %polya_1_distribution random number distribution
-//     * @p x from the input stream @p is.
-//     *
-//     * @param is An input stream.
-//     * @param x A %polya_1_distribution random number
-//     *            generator engine.
-//     *
-//     * @returns The input stream with @p x extracted or in an error
-//     *          state.
-//     */
-//    template<typename IntType1>
-//	friend std::istream&
-//	operator>>(std::istream& is,
-//		   polya_1_distribution<IntType1>& x) {
-//        return is
-//            >> x.m_param.red_n
-//            >> x.m_param.black_n
-//            >> x.m_param.c
-//            >> x.m_param.c
-//            >> x.m_param.size;
-//    }
-//
-//    param_type m_param;
-//};
-
 template <class IntType = int>
-std::discrete_distribution<IntType> polya_1_distribution(IntType black_n, IntType red_n, IntType c, IntType size) {
+std::discrete_distribution<IntType> polya_1(IntType black_n, IntType red_n, IntType c, IntType size) {
     std::vector<double> probabilities;
     probabilities.reserve(size + 1);
     double p0 = 1;
@@ -159,9 +18,9 @@ std::discrete_distribution<IntType> polya_1_distribution(IntType black_n, IntTyp
     double p = p0;
     probabilities.push_back(p);
     for (int x = 1; x <= size; ++x) {
-        p *= (size + 1 - x) * (black_n + (x - 1) * c);
+        p *= (size - (x - 1)) * (black_n + (x - 1) * c);
         p /= x * (red_n + (size - x) * c);
         probabilities.push_back(p);
     }
-    return std::discrete_distribution<IntType>(probabilities.begin(), probabilities.end());
+    return { probabilities.begin(), probabilities.end() };
 }
